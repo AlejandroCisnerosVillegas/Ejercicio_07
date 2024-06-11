86abbf3b130af22cac4a216972ec9a47409e4f42
Cristian Alejandro Cisneros Villegas <125778068+AlejandroCisnerosVillegas@users.noreply.github.com>
2024-06-11 14:25:41 -0600

    Add files via upload

--- login_registrar.php
+++ login_registrar.php
@@ -1,38 +1,28 @@
 <?php
-
-include("conexion.php");
-
-$nombre = $_POST["usuario"];
-$pass   = $_POST["pass"];
-
-//Login
-if(isset($_POST["btningresar"]))
-{
-	$query = mysqli_query($conn,"SELECT * FROM poject_08_login WHERE usuario = '$nombre' AND password='$pass'");
-	$nr = mysqli_num_rows($query);
-	
-	if($nr==1)
+	include("conexion.php");
+	$nombre = $_POST["usuario"];
+	$pass   = $_POST["pass"];
+	if(isset($_POST["btningresar"]))
 	{
-		echo "<script> alert('Bienvenido $nombre'); window.location='principal.html' </script>";
-	}else
-	{
-		echo "<script> alert('Usuario no existe'); window.location='index.html' </script>";
+		$query = mysqli_query($conn,"SELECT * FROM poject_08_login WHERE usuario = '$nombre' AND password='$pass'");
+		$nr = mysqli_num_rows($query);
+		if($nr==1)
+		{
+			echo "<script> alert('Bienvenido $nombre'); window.location='principal.html' </script>";
+		}else
+		{
+			echo "<script> alert('Usuario no existe'); window.location='index.html' </script>";
+		}
 	}
-}
-
-//Registrar
-if(isset($_POST["btnregistrar"]))
-{
-	$sqlgrabar = "INSERT INTO poject_08_login(usuario,password) values ('$nombre','$pass')";
-	
-	if(mysqli_query($conn,$sqlgrabar))
+	if(isset($_POST["btnregistrar"]))
 	{
-		echo "<script> alert('Usuario registrado con exito: $nombre'); window.location='index.html' </script>";
-	}else 
-	{
-		echo "Error: ".$sqlgrabar."<br>".mysql_error($conn);
+		$sqlgrabar = "INSERT INTO poject_08_login(usuario,password) values ('$nombre','$pass')";
+		if(mysqli_query($conn,$sqlgrabar))
+		{
+			echo "<script> alert('Usuario registrado con exito: $nombre'); window.location='index.html' </script>";
+		}else 
+		{
+			echo "Error: ".$sqlgrabar."<br>".mysql_error($conn);
+		}
 	}
-}
-
-
 ?>
